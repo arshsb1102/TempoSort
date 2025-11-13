4696568794dcb65491ff22fe807ab12b92b12c5f
arshsb1102 <arshdeepsbhatia11@gmail.com>
2025-11-13 07:08:56 +0530

    added resend

--- NotificationService.DataAccess/SmtpService/SmtpService.cs
+++ NotificationService.DataAccess/SmtpService/SmtpService.cs
@@ -4,32 +4,45 @@ using MimeKit;
 using Microsoft.Extensions.Options;
 using NotificationService.DataAccess.Interfaces;
 using NotificationService.Models.SMTP;
+using Resend;
+using Microsoft.Extensions.Configuration;
 
 namespace NotificationService.DataAccess.SmtpService;
 
-public class SmtpService : ISmtpService
+public class SmtpService(IOptions<SmtpSettings> options, IConfiguration _config) : ISmtpService
 {
-    private readonly SmtpSettings _settings;
-
-    public SmtpService(IOptions<SmtpSettings> options)
-    {
-        _settings = options.Value;
-    }
-
+    private readonly SmtpSettings _settings = options.Value;
+    private readonly IConfiguration config = _config;
+    private readonly bool isHttp = _config.GetValue<bool>("UseResendHttpClient");
     public async Task SendAsync(string toEmail, string subject, string htmlBody)
     {
-        var email = new MimeMessage();
-        email.From.Add(new MailboxAddress(_settings.FromName, _settings.FromEmail));
-        email.To.Add(MailboxAddress.Parse(toEmail));
-        email.Subject = subject;
+        if (isHttp)
+        {
+            var apiKey = config["Resend:Api_Key"] ?? throw new Exception("Resend API Key is not configured");
+            IResend resend = ResendClient.Create(apiKey);
+            var resp = await resend.EmailSendAsync(new EmailMessage()
+            {
+                From = $"{_settings.FromName} <{_settings.FromEmail}>",
+                To = toEmail,
+                Subject = subject,
+                HtmlBody = htmlBody
+            });
+        }
+        else
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(_settings.FromName, _settings.FromEmail));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
 
-        var builder = new BodyBuilder { HtmlBody = htmlBody };
-        email.Body = builder.ToMessageBody();
+            var builder = new BodyBuilder { HtmlBody = htmlBody };
+            email.Body = builder.ToMessageBody();
 
-        using var smtp = new SmtpClient();
-        await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
-        await smtp.AuthenticateAsync(_settings.Username, _settings.Password);
-        await smtp.SendAsync(email);
-        await smtp.DisconnectAsync(true);
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_settings.Username, _settings.Password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
     }
 }
